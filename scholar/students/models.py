7e181a6f4996be64aa8d98b90833bd2ddfb304b3
David <davidestebanabril96@gmail.com>
2023-10-27 20:10:37 -0500

    migracionesmodelo

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+class User(models.Model):
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    dni=models.CharField(max_length=10,default="")
+    age=models.IntegerField(default=0)
 # Create your models here.
+
+
